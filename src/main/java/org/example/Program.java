0c77ebe30b6986bcc5bca89e66f9eb447eff37c8
Gabryel Duarte <gabryelduartepessanha@gmail.com>
2026-07-14 16:05:52 -0300

    Conexao com o banco de dados

--- src/main/java/org/example/Program.java
+++ src/main/java/org/example/Program.java
@@ -1,14 +1,20 @@
 package org.example;
 
+import org.example.controller.Banco;
 import org.example.model.Atendimento;
 import org.example.model.Medico;
 import org.example.model.Paciente;
 import org.example.model.Telefone;
 
+import java.net.ConnectException;
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class Program {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws SQLException {
+        Banco b = new Banco();
+        Connection conexao = b.conectar();
+        b.desconectar(conexao);
     }
 }
--- src/main/java/org/example/controller/Banco.java
+++ src/main/java/org/example/controller/Banco.java
@@ -1,4 +1,59 @@
 package org.example.controller;
 
+import org.example.model.*;
+
+import java.net.ConnectException;
+import java.security.PublicKey;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Banco {
+    private String usuario;
+    private String senha;
+    private String url;
+
+    public Banco(){
+
+    }
+
+    public Connection conectar(){
+        usuario="root";
+        senha="root";
+        url="jdbc:mysql://localhost:3306/vidanova";
+
+        Connection conectado = null;
+
+        try{
+            conectado = DriverManager.getConnection(url, usuario, senha);
+            System.out.println("Banco de dados conectado com sucesso!");
+        }catch(SQLException e){
+            System.out.println("Não foi possível conectar ao banco de dados com essas informações.");
+        }
+        return conectado;
+    }
+
+    public void desconectar(Connection conexao) throws SQLException {
+        conexao.close();
+    }
+
+    public void adicionar(Medico medico, Connection conexao){
+
+    }
+
+    public void adicionar(Paciente paciente, Connection conexao){
+
+    }
+
+    public void adicionar(Endereco endereco, Connection conexao){
+
+    }
+
+    public void adicionar(Telefone telefone, Connection conexao){
+
+    }
+
+    public void adicionar(Atendimento atendimento, Connection conexao){
+
+    }
 }
